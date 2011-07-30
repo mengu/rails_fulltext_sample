f0eb510b460e5f2fb835c58bfe6f7d0c16aa4e48
Mengu Kagan <mengukagan@gmail.com>
2011-07-30 18:35:44 +0300

    generated the company scaffold.

---
+++ db/migrate/20110730153531_create_companies.rb
@@ -0,0 +1,16 @@
+class CreateCompanies < ActiveRecord::Migration
+  def self.up
+    create_table :companies do |t|
+      t.string :name
+      t.string :city
+      t.string :state
+      t.string :country
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :companies
+  end
+end
